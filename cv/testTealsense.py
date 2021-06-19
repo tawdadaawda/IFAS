7680005dc608439d9bc247c3b30d92ac031a1da3
tawdadaawda <pmgbpm50@gmail.com>
2021-06-19 12:03:52 +0900

    テストファイル修正

--- cv/testTealsense.py
+++ cv/testTealsense.py
@@ -1,5 +1,5 @@
 from numpy import real, true_divide
-from IFAS.cv.realsense import Realsense
+from realsense import Realsense
 import realsense
 import cv2
 
@@ -8,7 +8,7 @@ realsense.configurePipeline()
 realsense.startStream()
 while True:
     color_img, bg_removed_img = realsense.getFrame()
-    cv2.imshow(color_img)
+    cv2.imshow("image", color_img)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
